48bbc78b75010475425b59d0cdd199d232ffbe87
Sebastián Ignacio Núñez Llantén <seb.nunez@duocuc.cl>
2024-01-11 15:25:12 -0300

    paso variables
    se hizo terrible feo el paso de variables pero funciona

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( public appComponent: AppComponent) {}
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators,FormBuilder} from '@angular/forms';
+import { Router,NavigationExtras } from '@angular/router';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup;
+  constructor(public fb:FormBuilder,private router: Router,private appComponent:AppComponent ) { 
+    this.formLogin=this.fb.group({
+      'usuario':new FormControl("",Validators.required),
+      'password':new FormControl("",Validators.required),
+    });
+  }
+  ingresar(){
+    this.appComponent.usuario=this.formLogin.value['usuario'];
+    this.router.navigate(['/home']);
+  };
 
   ngOnInit() {
   }
